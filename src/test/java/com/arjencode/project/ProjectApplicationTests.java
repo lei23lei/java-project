de6f131199d783d9c8a670620d2707ceec950aec
peter <lei23lei91@gmail.com>
2025-07-27 10:31:29 -0400

    first

---
+++ src/test/java/com/arjencode/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjencode.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
